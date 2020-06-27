7870c21009f8520f1e62babbee2e95fbd373a509
Siva Nagi Reddy <siva.alla@outlook.com>
2020-06-27 15:44:31 +0530

    consul discovery implemented

--- client-one/src/main/java/com/sivanagireddy/clientone/client/CarClient.java
+++ client-one/src/main/java/com/sivanagireddy/clientone/client/CarClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "car", url = "http://localhost:8080")
+@FeignClient("service-one")
 public interface CarClient {
 
     @GetMapping("/cars/{id}")
